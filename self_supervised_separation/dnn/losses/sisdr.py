f356fc6a21b23334e8654c35dac84bd503bf5d39
Efthymios Tzinis <etzinis2@illinois.edu>
2020-10-08 19:06:39 -0500

    Add loss sisdr with reweighted samples

--- self_supervised_separation/dnn/losses/sisdr.py
+++ self_supervised_separation/dnn/losses/sisdr.py
@@ -61,7 +61,7 @@ def pit_loss( y, z, of=0, SI=False):
     return -s[j,i].mean()
 
 
-class PermInvariantSISDR(nn.Module):
+class HigherOrderPermInvariantSISDR(nn.Module):
     """!
     Class for SISDR computation between reconstructed signals and
     target wavs by also regulating it with learned target masks."""
@@ -72,7 +72,8 @@ class PermInvariantSISDR(nn.Module):
                  n_sources=None,
                  backward_loss=True,
                  improvement=False,
-                 return_individual_results=False):
+                 return_individual_results=False,
+                 var_weight=0.0):
         """
         Initialization for the results and torch tensors that might
         be used afterwards
@@ -90,6 +91,7 @@ class PermInvariantSISDR(nn.Module):
         self.improvement = improvement
         self.n_sources = n_sources
         self.return_individual_results = return_individual_results
+        self.var_weight=var_weight
 
     def normalize_input(self, pr_batch, t_batch, initial_mixtures=None):
         min_len = min(pr_batch.shape[-1],
@@ -148,10 +150,158 @@ class PermInvariantSISDR(nn.Module):
             base_sisdr = self.compute_permuted_sisnrs(initial_mix,
                                                       t_batch,
                                                       t_t_diag, eps=eps)
-            best_sisdr -= base_sisdr.mean()
+            best_sisdr -= base_sisdr.mean([-2, -1])
+
+        running_mean = best_sisdr.mean()
+        print(running_mean)
+        print(best_sisdr)
+        # var_loss = torch.mean((best_sisdr - running_mean)**2, dim=-1)
+        weights = torch.zeros_like(best_sisdr)
+        weights[torch.argmin(best_sisdr)] = 1.
+        var_loss = torch.sum(weights * best_sisdr)
+        print(var_loss)
+
+        # if self.reweighted is not None:
+        #     print(best_sisdr.mean())
+        #     new_weights = torch.softmax(- best_sisdr / 20., 0)
+        #     best_sisdr = new_weights * best_sisdr
+        #     print(best_sisdr)
 
         if not self.return_individual_results:
             best_sisdr = best_sisdr.mean()
+
+        if self.backward_loss:
+            return - best_sisdr - self.var_weight * var_loss
+        return best_sisdr
+
+    def forward(self,
+                pr_batch,
+                t_batch,
+                eps=1e-9,
+                initial_mixtures=None):
+        """!
+        :param pr_batch: Reconstructed wavs: Torch Tensors of size:
+                         batch_size x self.n_sources x length_of_wavs
+        :param t_batch: Target wavs: Torch Tensors of size:
+                        batch_size x self.n_sources x length_of_wavs
+        :param eps: Numerical stability constant.
+        :param initial_mixtures: Initial Mixtures for SISDRi: Torch Tensor
+                                 of size: batch_size x 1 x length_of_wavs
+
+        :returns results_buffer Buffer for loading the results directly
+                 to gpu and not having to reconstruct the results matrix: Torch
+                 Tensor of size: batch_size x 1
+        """
+        pr_batch, t_batch, initial_mixtures = self.normalize_input(
+            pr_batch, t_batch, initial_mixtures=initial_mixtures)
+
+        sisnr_l = self.compute_sisnr(pr_batch,
+                                     t_batch,
+                                     eps=eps,
+                                     initial_mixtures=initial_mixtures)
+
+        return sisnr_l
+
+
+class PermInvariantSISDR(nn.Module):
+    """!
+    Class for SISDR computation between reconstructed signals and
+    target wavs by also regulating it with learned target masks."""
+
+    def __init__(self,
+                 batch_size=None,
+                 zero_mean=False,
+                 n_sources=None,
+                 backward_loss=True,
+                 improvement=False,
+                 return_individual_results=False,
+                 reweighted=None):
+        """
+        Initialization for the results and torch tensors that might
+        be used afterwards
+
+        :param batch_size: The number of the samples in each batch
+        :param zero_mean: If you want to perform zero-mean across
+        last dimension (time dim) of the signals before SDR computation
+        """
+        super().__init__()
+        self.bs = batch_size
+        self.perform_zero_mean = zero_mean
+        self.backward_loss = backward_loss
+        self.permutations = list(itertools.permutations(
+            torch.arange(n_sources)))
+        self.improvement = improvement
+        self.n_sources = n_sources
+        self.return_individual_results = return_individual_results
+        self.reweighted=reweighted
+
+    def normalize_input(self, pr_batch, t_batch, initial_mixtures=None):
+        min_len = min(pr_batch.shape[-1],
+                      t_batch.shape[-1])
+        if initial_mixtures is not None:
+            min_len = min(min_len, initial_mixtures.shape[-1])
+            initial_mixtures = initial_mixtures[:, :, :min_len]
+        pr_batch = pr_batch[:, :, :min_len]
+        t_batch = t_batch[:, :, :min_len]
+
+        if self.perform_zero_mean:
+            pr_batch = pr_batch - torch.mean(
+                pr_batch, dim=-1, keepdim=True)
+            t_batch = t_batch - torch.mean(
+                t_batch, dim=-1, keepdim=True)
+            if initial_mixtures is not None:
+                initial_mixtures = initial_mixtures - torch.mean(
+                    initial_mixtures, dim=-1, keepdim=True)
+        return pr_batch, t_batch, initial_mixtures
+
+    @staticmethod
+    def dot(x, y):
+        return torch.sum(x * y, dim=-1, keepdim=True)
+
+    def compute_permuted_sisnrs(self,
+                                permuted_pr_batch,
+                                t_batch,
+                                t_t_diag, eps=10e-8):
+        s_t = (self.dot(permuted_pr_batch, t_batch) /
+               (t_t_diag + eps) * t_batch)
+        e_t = permuted_pr_batch - s_t
+        sisnrs = 10 * torch.log10(self.dot(s_t, s_t) /
+                                  (self.dot(e_t, e_t) + eps))
+        return sisnrs
+
+    def compute_sisnr(self,
+                      pr_batch,
+                      t_batch,
+                      initial_mixtures=None,
+                      eps=10e-8):
+
+        t_t_diag = self.dot(t_batch, t_batch)
+
+        sisnr_l = []
+        for perm in self.permutations:
+            permuted_pr_batch = pr_batch[:, perm, :]
+            sisnr = self.compute_permuted_sisnrs(permuted_pr_batch,
+                                                 t_batch,
+                                                 t_t_diag, eps=eps)
+            sisnr_l.append(sisnr)
+        all_sisnrs = torch.cat(sisnr_l, -1)
+        best_sisdr = torch.max(all_sisnrs.mean(-2), -1)[0]
+
+        if self.improvement:
+            initial_mix = initial_mixtures.repeat(1, self.n_sources, 1)
+            base_sisdr = self.compute_permuted_sisnrs(initial_mix,
+                                                      t_batch,
+                                                      t_t_diag, eps=eps)
+            best_sisdr -= base_sisdr.mean([-2, -1])
+
+        if self.reweighted is not None:
+            print(best_sisdr.mean())
+            new_weights = torch.softmax(- best_sisdr / 20., 0)
+            best_sisdr = new_weights * best_sisdr
+            print(best_sisdr)
+
+        if not self.return_individual_results:
+            best_sisdr = best_sisdr.sum()
 
         if self.backward_loss:
             return - best_sisdr
