e99754624eb1686630d4d7fe48c4d9567408ef91
Efthymios Tzinis <etzinis2@illinois.edu>
2020-10-13 01:28:22 -0500

    Adding configuration default path for esc 50

--- __config__.py
+++ __config__.py
@@ -1,6 +1,6 @@
 import os
 WHAM_ROOT_PATH = '/mnt/data/wham'
 LIBRI2MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri2Mix'
-ESC50_ROOT_PATH = 'hierarchical_sound_datasets/ESC50_partitioned'
+ESC50_ROOT_PATH = '/mnt/nvme/hierarchical_sound_datasets/ESC50_partitioned'
 
 API_KEY = 'dmhVcHSYCxktP4J5O917jeNA3'
